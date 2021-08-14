0e7ad51651f5c361b7d742257ba749386af7d538
gargiaero <67362869+gargiaero@users.noreply.github.com>
2021-08-14 12:41:48 +0530

    Add files via upload

---
+++ August 2021/14thauguts2021/prime_under10.py
@@ -0,0 +1,18 @@
+def sum_of_primes(list1):
+    s = 0
+    for num in list1:
+        i = 2
+        p = 1
+        while i <= num / 2:
+            if num % i == 0:
+                p = 0
+                break
+            i = i + 1
+        if p == 1:
+            s = s + num
+    return s
+
+
+list1 = [2,3,5,7]
+s = sum_of_primes(list1)
+print("Sum of all prime numbers:", s)
